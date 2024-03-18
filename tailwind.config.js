2f63da6e708f154d3ac9c3293b998473dd6af588
hatrnuhn <yusron.ibnunaim@gmail.com>
2024-03-19 06:12:16 +0700

    fix hero, and add src/ and tailwind.config.js to git

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/input.css"],
+  theme: {
+    colors: {
+      "bcr-blue": "#0D28A6",
+      "bcr-lighterblue": "#F1F3FF",
+      "bcr-limegreen": "#5CB85F",
+    },
+    width: {
+      "91prcnt": "91.111%",
+      "81prcnt": "81.111%",
+      "screen": "100vw",
+      "full": "100%",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
