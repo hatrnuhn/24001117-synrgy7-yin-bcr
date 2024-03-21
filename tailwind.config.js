3d6671406adbce07d3afbc1cdb639dee26cc5cac
hatrnuhn <yusron.ibnunaim@gmail.com>
2024-03-21 21:04:56 +0700

    feat(cta): add desktop-view for cta section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,30 +2,34 @@
 module.exports = {
   content: ["./index.html", "./src/input.css"],
   theme: {
-    colors: {
-      "bcr-blue": "#0D28A6",
-      "bcr-lighterblue": "#F1F3FF",
-      "bcr-slighterlightblue":"#CFD4ED",
-      "bcr-limegreen": "#5CB85F",
-      "bcr-danger":"#FA2C5A",
-      "bcr-warning":"#F9CC00",
-      "bcr-success":"#73CA5C",
-      "bcr-neutral02":"#D0D0D0",
-      "white":"#FFFFFF",
-      "bcr-dark":"#222222",
+    extend: {
+      colors: {
+        "bcr-blue": "#0D28A6",
+        "bcr-lighterblue": "#F1F3FF",
+        "bcr-slighterlightblue":"#CFD4ED",
+        "bcr-limegreen": "#5CB85F",
+        "bcr-danger":"#FA2C5A",
+        "bcr-warning":"#F9CC00",
+        "bcr-success":"#73CA5C",
+        "bcr-neutral02":"#D0D0D0",
+        "white":"#FFFFFF",
+        "bcr-dark":"#222222",
+      },
+      width: {
+        "91prcnt": "91.111%",
+        "81prcnt": "81.111%",
+        "screen": "100vw",
+        "full": "100%",
+        "half":"50%",
+        "auto":"auto",
+      },
+      borderWidth: {
+        "1":"1px",
+      },
+      gap: {
+        "13":"52px",
+      },  
     },
-    width: {
-      "91prcnt": "91.111%",
-      "81prcnt": "81.111%",
-      "screen": "100vw",
-      "full": "100%",
-      "half":"50%",
-      "auto":"auto",
-    },
-    borderWidth: {
-      "1":"1px",
-    },
-    extend: {},
   },
   plugins: [],
 }
